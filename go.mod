68133a8fa2b637c93d3e3cd85dc6bea23cf9f66b
rin1208 <sora1208sky@gmail.com>
2020-03-25 02:49:00 +0900

    add db

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rin1208/k8s-study
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/jinzhu/gorm v1.9.12
+)
--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+type Names struct {
+	Name string
+}
 
 func main() {
 	r := gin.Default()
+	r.GET("/", huga)
 	r.GET("/hoge", hoge)
 	r.Run(":80")
 }
 func hoge(c *gin.Context) {
-	c.JSON(200, "hgoe")
+	client, err := gorm.Open("mysql", "root:root@tcp([mysql]:3306)/hoge")
+
+	client.AutoMigrate(&Names{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	var hunga Names
+	hunga.Name = "hoge"
+	client.Create(&hunga)
+	c.JSON(200, "hoge")
+}
+
+func huga(c *gin.Context) {
+	client, err := gorm.Open("mysql", "root:root@tcp([mysql]:3306)/hoge")
+	client.AutoMigrate(&Names{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	var hunga Names
+	client.Find(&hunga)
+	c.JSON(200, hunga)
 }
